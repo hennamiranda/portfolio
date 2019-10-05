c09b905d09dee8db27b82f000e63b3c11765ff0f
hennamiranda <hemi.heiskanen@gmail.com>
2019-10-05 12:26:14 +0200

    Portfolio Version 1

---
+++ main.js
@@ -0,0 +1,20 @@
+window.onload = () => {
+    const backgroundFade = document.querySelector('#background-fade');
+    const openMenuButton = document.querySelector('#button-open');
+    const closeMenuButton = document.querySelector('#button-close');
+    const nav = document.querySelector('nav');
+  
+    openMenuButton.addEventListener('click', () => {
+      nav.setAttribute('class', 'open');
+      backgroundFade.setAttribute('class', 'show');
+    });
+  
+    closeMenuButton.addEventListener('click', () => {
+      nav.setAttribute('class', 'close');
+      backgroundFade.setAttribute('class', 'hide');
+    });
+    backgroundFade.addEventListener('click', () => {
+        nav.setAttribute('class', 'close');
+        backgroundFade.setAttribute('class', 'hide');
+      });
+  };
