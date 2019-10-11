819a506f3316627f05af755065613559bdc75289
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2019-10-11 12:09:23 +0200

    refactor js

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
 window.onload = () => {
+    // for animations
+    AOS.init({
+        duration: 1500,
+        once: true,
+    });
+
+    // manage open and close nav
     const backgroundFade = document.querySelector('#background-fade');
     const openMenuButton = document.querySelector('#button-open');
     const closeMenuButton = document.querySelector('#button-close');
     const nav = document.querySelector('nav');
-  
+
     openMenuButton.addEventListener('click', () => {
-      nav.setAttribute('class', 'open');
-      backgroundFade.setAttribute('class', 'show');
+        nav.setAttribute('class', 'open');
+        backgroundFade.setAttribute('class', 'show');
     });
-  
+
     closeMenuButton.addEventListener('click', () => {
-      nav.setAttribute('class', 'close');
-      backgroundFade.setAttribute('class', 'hide');
+        nav.setAttribute('class', 'close');
+        backgroundFade.setAttribute('class', 'hide');
     });
     backgroundFade.addEventListener('click', () => {
         nav.setAttribute('class', 'close');
         backgroundFade.setAttribute('class', 'hide');
-      });
-  };
+    });
+};
